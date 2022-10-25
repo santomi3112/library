10a747437574d06126b02589d965e32685743fcb
santomi3112 <adiprasetio007@gmail.com>
2022-10-25 15:54:34 +0700

    bingung

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let library = [
   {
     title: "Pale Blue Dot",
     author: "Carl Sagan",
-    "publish date": 1994,
+    publish: 1994,
     read: "not",
   },
   {
-    title: "Pale Blue Dot",
-    author: "Carl Sagan",
-    "publish date": 1994,
+    title: "Modern Chemistry",
+    author: "H. Clark Metcalfe",
+    publish: 1974,
     read: "not",
   },
 ];
+
+const tbody = document.querySelector("[data-tbody]");
 
 function Book() {
   // the constructor
@@ -21,11 +23,14 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+function showBookToTable() {
+  
+}
 
 function openForm() {
-  document.querySelector('[data-form]').style.display = "block"
+  document.querySelector("[data-form]").style.display = "block";
 }
 
 function closeForm() {
-  document.querySelector('[data-form]').style.display = "none"
+  document.querySelector("[data-form]").style.display = "none";
 }
